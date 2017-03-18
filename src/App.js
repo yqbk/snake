0c241555b8fcdf02bec9107282b759300aa3acf3
Jakub Syrek <jksyrek@gmail.com>
2017-03-18 12:13:28 +0100

    board class

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Board from './Board'
+
 class App extends Component {
   render() {
     return (
-      <div> {this.props.text} </div>
+      <Board size = '20' />
     );
   }
 }
